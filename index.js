f4e05b33abf9e38b4da97347cda0352100cb1f35
GOZLAN204 <gozlanghadir295@gmail.com>
2025-11-07 09:05:13 +0200

    added env

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express');
+require('dotenv').config();
 const app = express();
-const PORT = 3333;
+const PORT = process.env.PORT;
+const api=process.env.HOST;
 
 app.get('/', (req, res) => {
   res.send('Hello');
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on http://${api}:${PORT}`);
 });
 //gg
