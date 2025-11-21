722c11cd028bf5c3961ecd9652ee83648a2ebb4e
GOZLAN204 <gozlanghadir295@gmail.com>
2025-11-21 08:41:10 +0200

    added delete

--- model/users_M.js
+++ model/users_M.js
@@ -1,25 +1,25 @@
 const db = require('../config/db_config');
 
-async function getAll() {
-    let sql = `SELECT id, name, email FROM users`;
-    let [rows] = await db.query(sql);
+async function getAll(){
+    let sql = `SELECT id,name,email FROM users`;
+    let [rows] = await db.query(sql);    
     return rows;
 }
 
-async function getOne(id) {
-    let sql = `SELECT id, name, email FROM users WHERE id = ?`;
-    let [result] = await db.query(sql, [id]);
+async function getOne(id){
+    let sql = `SELECT id,name,email FROM users WHERE id = ?`;
+    let [result] = await db.query(sql,[id]);    
     return result[0];
 }
 
-async function deleteUser(id) {
+async function remove(id){
     let sql = `DELETE FROM users WHERE id = ?`;
-    let [result] = await db.query(sql, [id]);
-    return result;
+    let [result] = await db.query(sql,[id]);    
+    return result.affectedRows;
 }
 
-module.exports = {
+module.exports ={
     getAll,
     getOne,
-    deleteUser
-};
+    remove
+}
