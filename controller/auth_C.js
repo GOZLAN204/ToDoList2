550565597bf7d67339bea455f8647cbfe42b6efd
GOZLAN204 <gozlanghadir295@gmail.com>
2025-11-26 16:19:15 +0200

    added user with auth

--- controller/auth_C.js
+++ controller/auth_C.js
@@ -5,7 +5,30 @@ async function addUser(req,res) {
         res.status(500).json({message:"Server error"})
     }
 }
+const pool = require("../db_config/db_config");
+
+async function register(req, res) {
+    try {
+        const { name, email, userName } = req.body;
+        const pass = req.pass; // הסיסמה שכבר מוצפנת מה־middleware
+
+        const sql = `
+            INSERT INTO users (name, email, userName, pass)
+            VALUES (?, ?, ?, ?)
+        `;
+
+        await pool.execute(sql, [name, email, userName, pass]);
+
+        res.json({ message: "User registered successfully" });
+
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ error: "DB Error" });
+    }
+}
+
 
 module.exports ={
-    addUser
+    addUser,
+    register
 }
