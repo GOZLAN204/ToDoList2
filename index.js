fdbff17af000e1c204016e45d589c829178da121
GOZLAN204 <gozlanghadir295@gmail.com>
2025-12-05 10:39:18 +0200

    added categories

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ app.use(express.static(__dirname));
 app.use(express.json());
 app.use(cookies());
 
-app.get('/',(req,res)=>{res.sendFile(__dirname+'/public/index.html')})
-app.use('/users',require('./routes/users_R'));
+app.get('/',(req,res)=>{res.sendFile(__dirname+'/public/index.html')});
+app.use('/users',require('./routes/users_R.js'));
 app.use('/auth',require('./routes/auth_R'));
 
-app.listen(port,()=>{console.log(`http://${api}:${port}`)})
+app.listen(port,()=>{console.log(`http://${api}:${port}`)});
