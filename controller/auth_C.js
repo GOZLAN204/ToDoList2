ecef09968e456a10685e0d0a32abcde278ce1448
GOZLAN204 <gozlanghadir295@gmail.com>
2025-12-05 09:41:01 +0200

    added verify login

--- controller/auth_C.js
+++ controller/auth_C.js
@@ -1,34 +1,69 @@
-async function addUser(req,res) {
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const {addUser,getByUserName,getByEmail} = require('../model/users_M');
+
+async function register(req,res) {
     try{
+        let name = req.body.name;
+        let email = req.body.email;
+        let userName = req.body.userName;
+        let pass = req.pass;
 
+        let user = await getByUserName(userName);
+        if(user){
+            return res.status(409).json({message:"שם משתמש קיים במערכת"});
+        }
+        user = await getByEmail(email);
+        if(user){
+            return res.status(409).json({message:"אימייל קיים במערכת"});
+        }
+
+        let userId = await addUser({name,email,userName,pass});
+        if(!userId){
+            return res.status(500).json({message:"Server error"});
+        }
+        res.status(201).json({message:"נוסף בהצלחה"});
     }catch(err){
-        res.status(500).json({message:"Server error"})
-    }
-}
-const pool = require("../db_config/db_config");
-
-async function register(req, res) {
-    try {
-        const { name, email, userName } = req.body;
-        const pass = req.pass; // הסיסמה שכבר מוצפנת מה־middleware
-
-        const sql = `
-            INSERT INTO users (name, email, userName, pass)
-            VALUES (?, ?, ?, ?)
-        `;
-
-        await pool.execute(sql, [name, email, userName, pass]);
-
-        res.json({ message: "User registered successfully" });
-
-    } catch (err) {
-        console.log(err);
-        res.status(500).json({ error: "DB Error" });
+        console.error(err);
+        res.status(500).json({message:"Server error"});
     }
 }
 
+async function login(req,res,next) {
+    try{
+        let user = await getByUserName(req.body.userName);
+        if(!user){
+            return res.status(400).json({message:"שם משתמש או סיסמה שגויים"});
+        }
+        let isMatch = await bcrypt.compare(req.body.pass,user.pass);
+        if(!isMatch){
+            return res.status(400).json({message:"שם משתמש או סיסמה שגויים"});
+        }
+        req.user = user;
+        next();
+    }catch(err){
+        console.error(err);
+        res.status(500).json({message:"Server error"});
+    }
+}
+
+function createJwt(req,res) {
+    try{
+        let user = req.user;
+        let token = jwt.sign(
+            {id:user.id,name:user.name},
+            process.env.SECRET_KEY,
+            {expiresIn:'3h'}
+        );
+        res.cookie('jwt',token,{maxAge:1000*60*60*3}).status(200).json({message:"התחברת בהצלחה"});
+    }catch(err){
+        console.error(err);
+        res.status(500).json({message:"Server error"});
+    }
+}
 
 module.exports ={
-    addUser,
-    register
+    register,
+    login,
+    createJwt
 }
